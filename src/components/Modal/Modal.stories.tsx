5dd59974da60589e5e19ab7a0f55420d4fdd06c9
Bian2017 <bgl_cumt_zju@163.com>
2020-07-01 20:11:11 +0800

    feat: 完成模态框的样式还原

--- src/components/Modal/Modal.stories.tsx
+++ src/components/Modal/Modal.stories.tsx
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Button } from 'antd';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions'; // 记录组件行为
+
 import Modal from './Modal';
 
-const WrappedModal = () => {
+const TestModal = () => {
+  const [visible, setVisible] = useState(false);
+
   return (
     <div style={{ width: '400px' }}>
-      <Modal title="模态框" visible={true}>
-        hello world
+      <Button type="primary" onClick={() => setVisible(!visible)}>
+        打开模态框
+      </Button>
+
+      <Modal
+        title="模态框"
+        cancelText="取消"
+        okText="立即添加"
+        visible={visible}
+        onOk={() => setVisible(false)}
+        onCancel={() => setVisible(false)}
+      >
+        模态框内容
       </Modal>
     </div>
   );
@@ -15,4 +30,4 @@ const WrappedModal = () => {
 
 storiesOf('模态框', module)
   // 如果要自动显示组件的注释，需将当前 Story 的名称改成和组件名称一样
-  .add('Modal', WrappedModal);
+  .add('Modal', TestModal);
--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,47 +1,23 @@
 import React, { FC } from 'react';
-import { Modal, Button } from 'antd';
+import { Modal } from 'antd';
+import classNames from 'classnames';
 import { ModalProps } from 'antd/lib/modal';
-import classNames from 'classnames/bind';
 
-interface IIModalProps extends ModalProps {
-  /** 模态框标题 */
-  title: React.ReactNode;
-  /** 确定按钮文案 */
-  btnOkText?: string;
-  /** 加载状态 */
-  loading?: boolean;
-}
+const IModal: FC<ModalProps> = (props) => {
+  const { title, className, children, ...restProps } = props;
 
-const IModal: FC<IIModalProps> = (props) => {
-  const { title, children, onCancel, onOk, btnOkText = '确定', loading, className, ...restProps } = props;
-
-  const modalCls = classNames('commonModal', className);
+  const modalCls = classNames('ii-modal', className);
 
   return (
     <Modal
       data-testid="test-modal"
       title={<div style={{ textAlign: 'center' }}>{title}</div>}
       className={modalCls}
-      onCancel={onCancel}
-      onOk={onOk}
-      footer={[
-        <Button key="cancel" onClick={onCancel} className=".cancelBtn">
-          取消
-        </Button>,
-        <Button type="primary" key="create" onClick={onOk} className="btn" loading={loading}>
-          {btnOkText}
-        </Button>,
-      ]}
       {...restProps}
     >
       {children}
     </Modal>
   );
-};
-
-IModal.defaultProps = {
-  btnOkText: '确定',
-  loading: false,
 };
 
 export default IModal;

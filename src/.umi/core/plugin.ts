4f414d5aabd36b9d001fab359b5f4ee36db0937e
大黄蜂(张明瑞) <zhangmingrui@mail.i-i.ai>
2020-11-13 19:13:53 +0800

    feat: .umi 文件不提交

--- src/.umi/core/plugin.ts
+++
@@ -1,8 +0,0 @@
-// @ts-nocheck
-import { Plugin } from 'D:/job/ii-admin-ui/node_modules/@umijs/preset-built-in/node_modules/@umijs/runtime';
-
-const plugin = new Plugin({
-  validKeys: ['modifyClientRenderOpts','patchRoutes','rootContainer','render','onRouteChange',],
-});
-
-export { plugin };
